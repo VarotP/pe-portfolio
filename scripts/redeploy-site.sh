3c527587bff3346ee6266daf1a2a4f805bb1779b
VarotP <varotpava@gmail.com>
2026-07-08 21:59:50 +0700

    fix redeploy script

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -3,7 +3,7 @@
 tmux kill-session -t flask-app 2>/dev/null
 tmux new-session -d -s flask-app
 
-tmux send-keys -t flask-app "cd pe-portfolio" C-m
+tmux send-keys -t flask-app "cd .." C-m
 tmux send-keys -t flask-app "git fetch && git reset --hard origin/main" C-m
 tmux send-keys -t flask-app "source python3-virtualenv/bin/activate" C-m
 tmux send-keys -t flask-app "pip install -r requirements.txt" C-m
